40fa04c49234017d706de29ab0bae89e9030a72a
Denis John <denis.john.ndarake@gmail.com>
2019-08-27 12:01:02 +0100

    Create BookContext and

---
+++ src/context/BookContext.js
@@ -0,0 +1,27 @@
+import React, { createContext, useState } from "react";
+import uuid from "uuid/v1";
+
+export const BookContext = createContext();
+
+const BookContextProvider = props => {
+  const [books, setBook] = useState([
+    { title: "Book 1", author: "Author 1", id: uuid() },
+    { title: "Book 2", author: "Author 2", id: uuid() }
+  ]);
+
+  const addBook = (title, author) => {
+    setBook([...books, { title, author, id: uuid() }]);
+  };
+
+  const removeBook = id => {
+    books.filter(book => book.id !== id);
+  };
+
+  return (
+    <BookContext.Provider value={{ books, addBook, removeBook }}>
+      {props.children}
+    </BookContext.Provider>
+  );
+};
+
+export default BookContextProvider;
