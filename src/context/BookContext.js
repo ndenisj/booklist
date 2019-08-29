f748f9338ca9192422bbcd30466f0c75aaa1c63e
Denis John <denis.john.ndarake@gmail.com>
2019-08-29 22:15:15 +0100

    Changed code to use newly created Reducer

--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,24 +1,12 @@
-import React, { createContext, useState } from "react";
-import uuid from "uuid/v1";
+import React, { createContext, useReducer } from "react";
+import { bookReducer } from "../reducers/bookReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = props => {
-  const [books, setBook] = useState([
-    { title: "Book 1", author: "Author 1", id: uuid() },
-    { title: "Book 2", author: "Author 2", id: uuid() }
-  ]);
-
-  const addBook = (title, author) => {
-    setBook([...books, { title, author, id: uuid() }]);
-  };
-
-  const removeBook = id => {
-    setBook(books.filter(book => book.id !== id));
-  };
-
+  const [books, dispatch] = useReducer(bookReducer, []);
   return (
-    <BookContext.Provider value={{ books, addBook, removeBook }}>
+    <BookContext.Provider value={{ books, dispatch }}>
       {props.children}
     </BookContext.Provider>
   );
